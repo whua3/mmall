89d06e8a571b9dac5234ed93d71db5ad4522d339
whua3 <whua@hust.edu.cn>
2018-05-29 17:40:11 +0800

    shipping module finish

---
+++ src/main/java/com/mmall/controller/portal/ShippingController.java
@@ -0,0 +1,105 @@
+package com.mmall.controller.portal;
+
+import com.github.pagehelper.PageInfo;
+import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Shipping;
+import com.mmall.pojo.User;
+import com.mmall.service.IShippingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * @program: mmall
+ * @description:
+ * @author: whua
+ * @create: 2018-05-29 16:22
+ **/
+@Controller
+@RequestMapping("/shipping/")
+public class ShippingController {
+
+    @Autowired
+    private IShippingService iShippingService;
+
+    /**
+     * @Description: 新增收货地址
+     * @Param: [session, shipping]
+     * @return: com.mmall.common.ServerResponse
+     */
+    @RequestMapping("add.do")
+    @ResponseBody
+    public ServerResponse add(HttpSession session, Shipping shipping) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.add(user.getId(), shipping);
+    }
+
+    /**
+     * @Description: 删除收货地址
+     * @Param: [session, shippingId]
+     * @return: com.mmall.common.ServerResponse
+     */
+    @RequestMapping("del.do")
+    @ResponseBody
+    public ServerResponse del(HttpSession session, Integer shippingId) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.del(user.getId(), shippingId);
+    }
+
+    /**
+     * @Description: 更新收货地址
+     * @Param: [session, shippingId]
+     * @return: com.mmall.common.ServerResponse
+     */
+    @RequestMapping("update.do")
+    @ResponseBody
+    public ServerResponse update(HttpSession session, Shipping shipping) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.update(user.getId(), shipping);
+    }
+
+    /**
+     * @Description: 查询收货地址
+     * @Param: [session, shippingId]
+     * @return: com.mmall.common.ServerResponse
+     */
+    @RequestMapping("select.do")
+    @ResponseBody
+    public ServerResponse select(HttpSession session, Integer shippingId) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.select(user.getId(), shippingId);
+    }
+
+    /**
+     * @Description: 分页展示所有收货地址
+     * @Param: [pageNum, pageSize, session]
+     * @return: com.mmall.common.ServerResponse<com.github.pagehelper.PageInfo>
+     */
+    @RequestMapping("list.do")
+    @ResponseBody
+    public ServerResponse<PageInfo> list(@RequestParam(value = "pageNum", defaultValue = "1") int pageNum, @RequestParam(value = "pageSize", defaultValue = "10") int pageSize, HttpSession session) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.list(user.getId(), pageNum, pageSize);
+    }
+}
